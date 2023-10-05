3a65471f0076b4d4eae2933fbb57e6a8e2de1277
21dp2mlejn <martinslejn@gmail.com>
2023-10-05 16:01:21 +0300

    hz ko es daru bet kkas iet?!

---
+++ frontend/CitronZale/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import WelcomePage from '../views/Welcome.vue'
+import LoginPage from '../views/Login.vue'
+import RegisterPage from '../views/Register.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'welcome',
+      component: WelcomePage
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterPage
+    },
+  ]
+})
+
+export default router
