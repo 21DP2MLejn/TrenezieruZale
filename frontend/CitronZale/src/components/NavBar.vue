969f1385aa098f07c80c0797e8d5e82c644fe8e5
21dp2mlejn <martinslejn@gmail.com>
2023-10-05 21:27:18 +0300

    Navbar++

---
+++ frontend/CitronZale/src/components/NavBar.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="container">
+    <div class="image-container">
+      <img src="" alt="logo">
+    </div>
+    <button class="toggle-button" @click="toggleNav">
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+    </button>
+    <ul class="nav-list" :class="{ 'active': isNavOpen }">
+      <li><router-link to="">Prices</router-link></li>
+      <li><router-link to="">Schedule</router-link></li>
+      <li><router-link to="">Trainings</router-link></li>
+      <li><router-link to="">AboutUs</router-link></li>
+      <li><router-link to="">Clubs</router-link></li>
+      <li><router-link to="">Profile</router-link></li>
+    </ul>
+    <div class="register-button">
+      <button><router-link to="/register">Register</router-link></button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  background-color: var(--ShinyShamrock);
+  height: 8vh;
+  width: 100vw;
+  margin: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-bottom: 2px solid var(--TeaGreen);
+}
+
+.image-container {
+  background-color: var(--Black);
+  width: 4rem;
+  height: 4rem;
+  top: 0.3rem;
+  left: 0.3rem;
+  position: relative;
+}
+
+a {
+  text-decoration: none;
+  color: var(--Black);
+  font-size: large;
+}
+
+.register-button {
+  margin-left: auto;
+  position: absolute;
+  left: 110rem;
+  top: 1.3rem;
+
+}
+
+.register-button button {
+  
+}
+
+.nav-list {
+  width: 60vw;
+  height: 8vh;
+  list-style: none;
+  display: flex;
+  padding: 0;
+  position: relative;
+  left: 8rem;
+  top: -3.5rem;
+}
+
+.toggle-button {
+  display: none;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  z-index: 10000;
+  top: 0.2rem;
+  right: 1rem; 
+}
+
+li {
+  margin-right: 3rem;
+}
+
+.bar {
+  position: relative;
+  display: block;
+  width: 2rem;
+  height: 0.25rem;
+  top: 0.95rem;
+  margin: 0.4rem 0.1rem 0.20rem 0.5rem;
+  background-color: var(--Black);
+  transition: 0.4s;
+}
+
+/* Media query for mobile */
+@media (max-width: 992px) {
+  .toggle-button {
+    display: block;
+  }
+
+  .nav-list {
+    display: none;
+    flex-direction: column;
+    position: absolute;
+    top: 3.6rem;
+    left: 0;
+    width: 100%;
+    height: auto; /* Change the height to "auto" to accommodate variable content */
+    background-color: var(--ShinyShamrock);
+    text-align: center;
+    border-bottom: 2px solid var(--TeaGreen);
+  }
+
+  .nav-list.active {
+    display: flex;
+  }
+
+  /* Style for the active button (when the menu is open) */
+  .toggle-button.active .bar:nth-child(1) {
+    transform: rotate(-45deg) translate(-5px, 6px);
+  }
+
+  .toggle-button.active .bar:nth-child(2) {
+    opacity: 0;
+  }
+
+  .toggle-button.active .bar:nth-child(3) {
+    transform: rotate(45deg) translate(-5px, -6px);
+  }
+}
+</style>
+
+<script>
+export default {
+  name: 'NavBar',
+  data() {
+    return {
+      isNavOpen: false,
+    };
+  },
+  methods: {
+    toggleNav() {
+      this.isNavOpen = !this.isNavOpen;
+    },
+  },
+};
+</script>
