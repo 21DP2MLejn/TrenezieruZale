7b5be5bbbe1c1481e391bfc82fcdb9fea74f0077
21dp2mlejn <martinslejn@gmail.com>
2023-10-19 19:04:55 +0300

    prices + free trial

---
+++ frontend/CitronZale/src/components/FreeTrial.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container">
+        <div class="free-trial">
+            <h1>Not ready to start? Get 3 day free trial.</h1>
+            <p>By submitting this free training form, you agree to receive marketing information. You can withdraw your consent at any time by contacting help@citronzale.com .</p>
+            <input type="text" name="" id="" placeholder="Your email">
+            <button type="submit">GET FREE TRIAL</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'freetrial',
+}
+</script>
+
+<style scoped>
+
+.container{
+    width: 100vw;
+    height: auto;
+    background-color: var(--ShinyShamrock);
+    margin-bottom: 2rem;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    padding: 1rem;
+}
+
+input{
+    width: 16rem;
+    height: 2rem;
+    padding-left: 1rem;
+    border-radius: 1rem;
+    border: 0.15rem solid var(--PastelGreen);
+    margin-bottom: 1rem;
+    outline: none;
+  }
+
+  button{
+    width: 8rem;
+    height: 1.8rem;
+    border-radius: 1rem;
+    border: 0.15rem solid var(--PastelGreen);
+    margin-bottom: 1rem;
+    transition: 0.3s ease-in-out;
+    margin-left:  1rem; ;
+  }
+
+  button:hover{
+    transform: scale(1.1);
+}
+
+</style>
--- frontend/CitronZale/src/views/Prices.vue
+++ frontend/CitronZale/src/views/Prices.vue
@@ -6,69 +6,113 @@
     <div class="prices-container">
         <div class="card regular">
             <h2>Regular</h2>
+            <h3>Price: 14.99$</h3>
+            <p>Advantages</p>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>Unlimited visit 24/7</li>
+                <li>Membership freeze</li>
+                <li>Rewrite membership to another person</li>
+                <li>Minimal period of contract: 12months</li>
             </ul>
         </div>
         <div class="card flex">
             <h2>Flex</h2>
+            <h3>Price: 19.99$</h3>
+            <p>Advantages</p>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>Unlimited visit 24/7</li>
+                <li>Membership freeze</li>
+                <li>Rewrite membership to another person</li>
+                <li>Intro training</li>
+                <li>Minimal period of contract: 12months</li>
             </ul>
-        </div>
+
         </div>
         <div class="card super">
             <h2>Super</h2>
+            <h3>Price: 24.99$</h3>
+            <p>Advantages</p>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>Unlimited visit 24/7</li>
+                <li>Membership freeze</li>
+                <li>Rewrite membership to another person</li>
+                <li>Intro training</li>
+                <li>Group trainings</li>
+                <li>Sauna</li>
+                <li>Unlimited period of contract</li>
             </ul>
         </div>
         <div class="card deluxe">
             <h2>Deluxe</h2>
+            <h3>Price: 29.99$</h3>
+            <p>Advantages</p>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>Unlimited visit 24/7</li>
+                <li>Membership freeze</li>
+                <li>Rewrite membership to another person</li>
+                <li>Intro training</li>
+                <li>Group trainings</li>
+                <li>Sauna</li>
+                <li>Pool access</li>
+                <li>Water refill every 20 minutes</li>
+                <li>Unlimited period of contract</li>
             </ul>
         </div>
+    </div>
+    <div class="trial">
+        <FreeTrial />
+    </div>
 </main>
 </template>
 
 <script>
 import NavBar from '../components/NavBar.vue';
+import FreeTrial from '../components/FreeTrial.vue';
 export default {
     components:{
     NavBar,
+    FreeTrial,
 },
 }
 </script>
 
 <style scoped>
 
+input{
+    
+}
+  .prices-container {
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    flex-direction: row;
+    position: relative;
+    top: 5rem;
+    flex-wrap: wrap; 
+  }
+
+  .card {
+    max-width: 22rem; 
+    width: 100%; 
+    background-color: var(--ShinyShamrock);
+    position: relative;
+    margin: 2rem;
+    border-radius: 2rem;
+    padding: 1rem;
+  }
+
+  .trial{
+    position: relative;
+    top: 20rem;
+    left: 0;
+    margin: 0;
+    padding: 0;
+
+  }
+
+  @media (max-width: 768px) {
+    .card {
+      max-width: 100%;
+    }
+  }
 </style>
