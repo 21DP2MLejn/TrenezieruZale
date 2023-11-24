9d19afbada1df66479d564c1158bb7c4497943b7
21DP2MLejn <martinslejn@gmail.com>
2023-11-24 12:48:45 +0200

    profile

--- frontend/CitronZale/src/views/Profile.vue
+++ frontend/CitronZale/src/views/Profile.vue
@@ -1,15 +1,60 @@
 <template>
-    <div>
-        profile
+    <div class="navbar">
+        <NavBar />
+    </div>
+    <div class="container">
+        <div class="user-details">
+            <input type="text" class="name" placeholder="Name">
+            <input type="text" class="lastname" placeholder="Lastname">
+            <input type="date" class="birthdate" placeholder="Birthdate">
+        </div>
+        <div class="user-contact">
+            <input type="text" class="email" placeholder="E-mail">
+            <input type="text" class="phone-number" placeholder="Phone Number">
+        </div>
+        <div class="user-guardian">
+            <input type="text" class="guardian-name" placeholder="Guardian Name">
+            <input type="text" class="guardian-lastname" placeholder="Guardian Lastname">
+            <input type="text" class="guardian-email" placeholder="Guardian E-mail">
+            <input type="text" class="guardian-country" placeholder="Country" v-show="country">
+            <button id="choosecountry" @click="ShowCountry">Country</button>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    
+import NavBar from '../components/NavBar.vue';
+
+export default{
+    components:{
+    NavBar,
+},
+data(){
+    return{
+        country: false
+    }
+},
+method:{
+    ShowCountry(){
+        this.country = !this.country;
+
+    }
+}
+
 }
 </script>
 
 <style scoped>
+.container{
+    position: relative;
+}
+
+.navbar{
+    position: relative;
+    margin: 0;
+    padding: 0;
+    left: 0;
+    top: 0;
+}
 
 </style>
